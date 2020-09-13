fc9072e5580c24d6b762a1b880fe8f05179aed33
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-12 19:37:37 -0600

    header

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react';
+import logo from '../../images/amazon-logo.png'
+import './Header.css'
 
 class Header extends Component{
     render(){
         return(
-            <div>
-                Header.... With Even more header stuff...
+            <div className="header-wrapper">
+                <div className="header-top">
+                    <img className="header-top-logo" src={logo}></img>
+                    
+                    <div className="header-top-search">
+                        <div className="search-category">Cat</div>
+                        <input className="search-input"type="text"></input>
+                        <div className="search-icon">Search</div>
+                    </div>
+
+                    <div className="header-section-wrapper">
+                        <div className="header-language">Lang</div>
+                        <div className="header-section">
+                            <div className="section-top">Hello, Tyler</div>
+                            <div className="section-bottom">{`Accounts & Lists`}</div>
+                        </div>
+                        <div className="header-section">
+                            <div className="section-top">Returns</div>
+                            <div className="section-bottom">{`& Orders`}</div>
+                        </div>
+                        <div className="header-section">
+                        <div className="section-top"></div>
+                            <div className="section-bottom section-alone">Prime</div>
+                        </div>
+                        <div className="header-section">
+                            <div className="section-top"></div>
+                            <div className="section-bottom section-alone">Cart</div>
+                        </div>
+
+                    </div>
+                    
+
+                </div>
+                <div className="header-bottom">
+                    Bottom part of the header
+                </div>
             </div>
         )
     }
